064ea334e4b9821484295ba720a9dad2932aa862
aashikvilla <aashikvilla99@gmail.com>
2021-09-17 18:57:28 +0530

    update program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -17,19 +17,19 @@ namespace API
         public static async Task Main(string[] args)
         {
             var host=CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            var services= scope.ServiceProvider;
-            try{
-                var context=services.GetRequiredService<DataContext>();
-                context.Database.MigrateAsync();
-                await Seed.SeedData(context);
-            }
-            catch(Exception ex){
-                var logger=services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex,"error during migration");
-            }
+            // using var scope = host.Services.CreateScope();
+            // var services= scope.ServiceProvider;
+            // try{
+            //     var context=services.GetRequiredService<DataContext>();
+            //     context.Database.MigrateAsync();
+            //     await Seed.SeedData(context);
+            // }
+            // catch(Exception ex){
+            //     var logger=services.GetRequiredService<ILogger<Program>>();
+            //     logger.LogError(ex,"error during migration");
+            // }
 
-            host.RunAsync();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
